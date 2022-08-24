47e96dc72b59b18ef0df6bf197f121e53e5a9284
j-irion <juliusirion@gmx.de>
2022-08-24 22:47:02 +0200

    Import renderMenu, make menu button work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import "./style.css";
 import { renderHome } from "./home.js";
+import { renderMenu } from "./menu.js";
 
 renderHome();
 
 document.getElementById("home").addEventListener("click", () => {
   renderHome();
+});
+
+document.getElementById("menu").addEventListener("click", () => {
+  renderMenu();
 });
