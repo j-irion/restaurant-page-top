0cb9ae2508300a9a84c5aa0784f603faf7a11856
j-irion <juliusirion@gmx.de>
2022-08-25 17:01:18 +0200

    Write renderContact function

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,48 @@
 export function renderContact() {
-  console.log("test");
+  let content = document.getElementById("content");
+  content.innerHTML = "";
+
+  let contactDiv = document.createElement("div");
+  contactDiv.id = "contactDiv";
+  content.appendChild(contactDiv);
+
+  //Create address
+  let address = document.createElement("div");
+  address.id = "address";
+  contactDiv.appendChild(address);
+
+  let addressHeading = document.createElement("h2");
+  addressHeading.textContent = "Mail Address";
+  address.appendChild(addressHeading);
+
+  let addressContent = document.createElement("p");
+  addressContent.textContent =
+    "Ben Franklin 123 Sugarcane Lane, Candyville, Texas";
+  address.appendChild(addressContent);
+
+  //Create phone number
+  let tel = document.createElement("div");
+  tel.id = "tel";
+  contactDiv.appendChild(tel);
+
+  let telHeading = document.createElement("h2");
+  telHeading.textContent = "Phone Number";
+  tel.appendChild(telHeading);
+
+  let telContent = document.createElement("span");
+  telContent.textContent = "202-555-0191";
+  tel.appendChild(telContent);
+
+  //Create Email address
+  let email = document.createElement("div");
+  email.id = "email";
+  contactDiv.appendChild(email);
+
+  let emailHeading = document.createElement("h2");
+  emailHeading.textContent = "E-Mail Address";
+  email.appendChild(emailHeading);
+
+  let emailContent = document.createElement("span");
+  emailContent.textContent = "contact@awesome-restaurant.com";
+  email.appendChild(emailContent);
 }
